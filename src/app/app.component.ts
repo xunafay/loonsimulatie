f561dfe29f642b5f63972575c133d6716d2d315a
xunafay <me@xunafay.io>
2024-09-21 20:15:04 +0200

    first functional implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, model } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { PayScale, PayScaleService } from './pay-scale.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSelectModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'loonsimulatie';
+  selectedCategory = model<string | undefined>(undefined);
+  selectedScale = model<PayScale | undefined>(undefined);
+  selectedTrap = model<number | undefined>(undefined);
+
+  categories = computed(() => this.payScaleService.categories());
+  scales = computed(() => {
+    const scales = this.payScaleService.scales();
+    return this.selectedCategory() ? scales.filter(scale => scale.name.startsWith(this.selectedCategory()!)) : scales;
+  });
+  trappen = computed(() => {
+    let scale = this.scales().find(scale => scale.name == this.selectedScale()?.name);
+    return scale ? Array.from(Array(scale.salary.length).keys()) : [];
+  });
+
+  information = computed(() => {
+    const scale = this.selectedScale();
+    if (!scale) return '';
+    const trap = this.selectedTrap();
+    if (trap === undefined) return '';
+    return `De ${scale.name} schaal, trap ${trap} heeft een salaris van €${(scale.salary[trap] / 12 / 100).toFixed(2)}`;
+  });
+
+  constructor(
+    private payScaleService: PayScaleService
+  ) {
+    effect(() => {
+      console.log('trappen:', this.trappen());
+      console.log('selectedScale:', this.selectedScale());
+      console.log('scales:', this.scales());
+      console.log('selectedTrap:', this.selectedTrap());
+    });
+  }
 }
---
+++ src/app/pay-scale.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Signal, WritableSignal, computed, effect, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PayScaleService {
+  private readonly INDEX_FACTOR = 2.0807;
+
+  private _scales: WritableSignal<PayScale[]> = signal([]);
+  readonly scales: Signal<PayScale[]> = this._scales.asReadonly();
+
+  /**
+   * Categories are the first letter of the pay scale names, sorted alphabetically.
+   */
+  categories: Signal<string[]> = computed(() => {
+    let categories = Array.from(new Set(this.scales().map(scale => scale.name.at(0))));
+    categories.sort();
+    categories = categories.filter(cat => cat); // remove falsy values (undefined, empty strings)
+    return categories as string[];
+  });
+
+  constructor(private http: HttpClient) {
+    this.getPayScales();
+    effect(() => {
+      console.log('categories:', this.categories());
+    });
+  }
+
+  getPayScales(): void {
+    this.http.get('/data.csv', {
+      responseType: 'text' // prevent the default JSON parsing attempt
+    }).subscribe(data => {
+      this._scales.set(this.parseCSV(data));
+    });
+  }
+
+  private parseCSV(data: string): PayScale[] {
+    const rawRows = data.split('\n');
+      rawRows.shift(); // remove the header
+      let rows = rawRows.map(row => {
+        const [name, scale, salary] = row.split(',');
+        return { name, scale: parseInt(scale), salary: parseInt(salary) * this.INDEX_FACTOR };
+      });
+
+      // group rows by name
+      const scales: PayScale[] = [];
+      for (const row of rows) {
+        const scale = scales.find(s => s.name === row.name);
+        if (scale) {
+          scale.salary.push(row.salary);
+        } else {
+          scales.push({ name: row.name, salary: [row.salary] });
+        }
+      }
+
+      return scales;
+  }
+}
+
+export type PayScale = {
+  name: string;
+  salary: number[];
+};
