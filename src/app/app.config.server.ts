8b219468f90d0ddf664449a1488ddbd640721803
xunafay <me@xunafay.io>
2024-09-22 16:40:17 +0200

    fix: cache server side rendering http calls

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideClientHydration(withHttpTransferCacheOptions({})),
   ]
 };
 
